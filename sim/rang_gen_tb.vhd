f3214fe9820e2e942716cb4d72c59c91211e1a7f
Clayton Lawton <cla10law10@gmail.com>
2023-02-06 11:15:29 -0800

    Random # Generator
    Completed

---
+++ sim/rang_gen_tb.vhd
@@ -0,0 +1,59 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+use std.env.stop;
+
+entity rand_gen_tb is
+end rand_gen_tb;
+
+architecture Behavioral of rand_gen_tb is
+
+component rand_gen IS
+    PORT (
+        clk, rst : IN STD_LOGIC;
+        seed : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        output : OUT STD_LOGIC_VECTOR (3 DOWNTO 0)
+    );
+END component rand_gen;
+
+signal clk_tb : STD_LOGIC;
+signal rst_tb : STD_LOGIC;
+signal seed_tb : STD_LOGIC_VECTOR (7 DOWNTO 0);
+signal output_tb : STD_LOGIC_VECTOR (3 DOWNTO 0);
+
+constant CP : time := 10ns;
+
+begin
+
+uut : rand_gen 
+    port map (
+        clk => clk_tb,
+        rst => rst_tb,
+        seed => seed_tb,
+        output => output_tb
+    );
+
+    -- Clock generation process
+    clk_gen : PROCESS
+    BEGIN
+        clk_tb <= '0';
+        WAIT FOR CP/2;
+        clk_tb <= '1';
+        WAIT FOR CP/2;
+    END PROCESS;
+
+   -- Input vector
+    input_gen : process
+    BEGIN
+
+        wait for CP;
+        seed_tb <= "01001111";
+        wait for CP;
+        rst_tb <= '1';
+        wait for CP;
+        rst_tb <= '0';
+        wait for 5 * CP;
+        stop;
+    end process;
+
+end Behavioral;
--- src/rand_gen.vhd
+++ src/rand_gen.vhd
@@ -1,18 +1,35 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
-use IEEE.NUMERIC_STD.all;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
-entity rand_gen is
-    port
-    (
-        clk, rst : in std_logic;
-        seed     : in std_logic_vector(7 downto 0);
-        output   : out std_logic_vector (3 downto 0)
+ENTITY rand_gen IS
+    PORT (
+        clk, rst : IN STD_LOGIC;
+        seed : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        output : OUT STD_LOGIC_VECTOR (3 DOWNTO 0)
     );
-end rand_gen;
+END rand_gen;
 
-architecture Behavioral of rand_gen is
+ARCHITECTURE Behavioral OF rand_gen IS
 
-begin
+    SIGNAL currstate : STD_LOGIC_VECTOR (7 DOWNTO 0);
+    SIGNAL nextstate : STD_LOGIC_VECTOR (7 DOWNTO 0);
+    SIGNAL feedback : STD_LOGIC;
 
-end Behavioral;
+BEGIN
+
+    stateReg : PROCESS (clk, rst)
+    BEGIN
+        IF rst = '1' THEN
+            currstate <= seed;
+        ELSIF rising_edge (clk) THEN
+            currstate <= nextstate;
+        END IF;
+    END PROCESS;
+
+    feedback <= currstate(4) XOR currstate(3) XOR currstate(2) XOR currstate(0);
+    nextstate <= feedback & currstate(7 DOWNTO 1);
+
+    output <= currstate(7 DOWNTO 4);
+
+END Behavioral;
