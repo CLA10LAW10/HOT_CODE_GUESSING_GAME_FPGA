4ba92cfc1ac836b11eb98b06c8707670029df430
Clayton Lawton <cla10law10@gmail.com>
2023-02-09 16:54:11 -0800

    Comments Part 4

--- sim/game_top_tb.vhd
+++ sim/game_top_tb.vhd
@@ -11,32 +11,33 @@ ARCHITECTURE Behavioral OF number_guess_tb IS
 
     COMPONENT number_guess IS
         PORT (
-            clk : IN STD_LOGIC;
-            rst : IN STD_LOGIC;
-            show : IN STD_LOGIC;
-            enter : IN STD_LOGIC;
-            switches : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-            leds : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            red_led : OUT STD_LOGIC;
-            blue_led : OUT STD_LOGIC;
-            green_led : OUT STD_LOGIC
+            clk : IN STD_LOGIC;                             -- Input clock
+            rst : IN STD_LOGIC;                             -- Input rst, used to reset the game
+            show : IN STD_LOGIC;                            -- Input show to SHOW the answer to the player
+            enter : IN STD_LOGIC;                           -- Input enter, used to indicate a guessing value.
+            switches : IN STD_LOGIC_VECTOR (3 DOWNTO 0);    -- Input switches, used to guess the secret number
+            leds : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);       -- Output LEDS, used when having the number shows
+            red_led : OUT STD_LOGIC;                        -- Output RED LED, used to indicate a high number
+            blue_led : OUT STD_LOGIC;                       -- Output BLUE LED, used to indicate a low nubmer
+            green_led : OUT STD_LOGIC                       -- Output GREEN LED, used to inidcate the correct number
         );
     END COMPONENT number_guess;
 
-    SIGNAL clk_tb : STD_LOGIC;
-    SIGNAL rst_tb : STD_LOGIC;
-    SIGNAL show_tb : STD_LOGIC;
-    SIGNAL enter_tb : STD_LOGIC;
-    SIGNAL switches_tb : STD_LOGIC_VECTOR (3 DOWNTO 0);
-    SIGNAL leds_tb : STD_LOGIC_VECTOR (3 DOWNTO 0);
-    SIGNAL red_led_tb : STD_LOGIC;
-    SIGNAL blue_led_tb : STD_LOGIC;
-    SIGNAL green_led_tb : STD_LOGIC;
+    SIGNAL clk_tb : STD_LOGIC;                          -- Signal simulated clock 
+    SIGNAL rst_tb : STD_LOGIC;                          -- Signal simulated reset button
+    SIGNAL show_tb : STD_LOGIC;                         -- Signal simulated show button
+    SIGNAL enter_tb : STD_LOGIC;                        -- Signal simulated enter button
+    SIGNAL switches_tb : STD_LOGIC_VECTOR (3 DOWNTO 0); -- Signal simulated 4 bit input to guess secret number
+    SIGNAL leds_tb : STD_LOGIC_VECTOR (3 DOWNTO 0);     -- Signal simulated output LED
+    SIGNAL red_led_tb : STD_LOGIC;                      -- Signal simulated red output LED
+    SIGNAL blue_led_tb : STD_LOGIC;                     -- Signal simulated blue output LED
+    SIGNAL green_led_tb : STD_LOGIC;                    -- Signal simulated green output LED
 
     CONSTANT CP : TIME := 8ns;
 
 BEGIN
 
+    -- Instantiate the unit under test
     uut : number_guess
     PORT MAP(
         clk => clk_tb,
@@ -62,13 +63,16 @@ BEGIN
     -- Input vector
     input_gen : PROCESS
     BEGIN
+
+        -- Initialize to 0 and reset
+        rst_tb <= '1';
         show_tb <= '0';
         enter_tb <= '0';
         switches_tb <= "0000";
+        WAIT FOR 2000000 * CP;
 
-        WAIT FOR 2000000 * CP;
-        rst_tb <= '1';
-        WAIT FOR 2000000 * CP;
+        -- Reset to seed random number generator
+        -- Generate a random number
         rst_tb <= '0';
         WAIT FOR 2000000 * CP;
         rst_tb <= '1';
@@ -76,16 +80,20 @@ BEGIN
         rst_tb <= '0';
         WAIT FOR 2000000 * CP;
 
+        -- Show the secret number
         show_tb <= '1';
         WAIT FOR 2000000 * CP;
         show_tb <= '0';
         WAIT FOR 2000000 * CP;
 
+        -- Reset the secret value
         rst_tb <= '1';
         WAIT FOR 1885999 * CP;
         rst_tb <= '0';
         WAIT FOR 2000000 * CP;
 
+        -- Guess a low number
+        -- Oberve the blue LED
         switches_tb <= "0001";
         WAIT FOR 2000000 * CP;
 
@@ -94,6 +102,8 @@ BEGIN
         enter_tb <= '0';
         WAIT FOR 2000000 * CP;
 
+        -- Guess a high number 
+        -- Observe the red LED
         switches_tb <= "1111";
         WAIT FOR 2000000 * CP;
 
@@ -102,15 +112,17 @@ BEGIN
         enter_tb <= '0';
         WAIT FOR 2000000 * CP;
 
+        -- Guess the correct number
+        -- Observe the flashing green LED
         switches_tb <= "1101";
         WAIT FOR 2000000 * CP;
 
         enter_tb <= '1';
         WAIT FOR 2000000 * CP;
         enter_tb <= '0';
-        WAIT FOR 2000000 * CP;
+        WAIT FOR 50_000_000 * CP;
 
-        WAIT;
+        stop;
     END PROCESS;
 
 END Behavioral;
